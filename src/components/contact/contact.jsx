0f1cbe01cc884373d5c621219c51386090a9205a
Jinpeng Zhang <79638739+Jimpoz@users.noreply.github.com>
2023-04-03 16:39:50 +0200

    Completed contact me form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import './contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_9bh8mll', 'template_vnpsfrj', form.current, 'PQOvmaUGlXzOfknpe')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
   return (
     <section id='contact'>
-      <h2>Contact me!</h2>
-      <form>
-        <div className="form-field">
-          <label htmlFor="name">Your name:</label>
-          <input type="text" id="name" name="name" />
-        </div>
-
-        <div className="form-field">
-          <label htmlFor="subject">Your e-mail:</label>
-          <input type="text" id="subject" name="subject" />
-        </div>
-
-        <div className="form-field">
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message"></textarea>
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
+      <form ref={form} onSubmit={sendEmail}>
+        <h2>Contact me!</h2>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
     </section>
   );
 }
